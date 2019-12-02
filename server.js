dc0b05891a906e768d9f2aa089325834b1106b37
Matheus <matheusnun.ns@gmail.com>
2019-12-01 23:17:31 -0300

    change routes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = (process.env && process.env.PORT) || 5000;
 
 const temperaturesLog = [];
 
-app.get('/temperaturesLog', async (req, res) => {
+app.get('/', async (req, res) => {
     try {
         res.json({temperaturesLog});   
     } catch (error) {
